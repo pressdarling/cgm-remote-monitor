391766fd2708363f7452774b807f3c98523d2a43
Navid <51497406+Navid200@users.noreply.github.com>
2024-01-29 16:27:30 -0500

    Update FreednsLogin.sh
    Using the new log utility for creating the logs

--- FreednsLogin.sh
+++ FreednsLogin.sh
@@ -5,6 +5,7 @@ echo "Log into the FreeDNS site. - Navid200"
 echo
 
 # This could also run in the background.  So, it should contain no dialog.
+# All the logs this utility generates are stored in an auto-login dedicated log file separate from the main system logs.
 
 freedns=$(wget --spider -S "https://freedns.afraid.org/" 2>&1 | awk '/HTTP\// {print $2}') # This will be 200 if FreeDNS is up.
 if [ $freedns -eq 200 ]  # Run the following only if FreeDNS is up.
@@ -18,30 +19,28 @@ then
     arg2=$(echo -n "$user|$pass" | sha1sum | awk '{print $1;}')
     arg="$arg1$arg2"
     wget -O /tmp/hosts "$arg"
-    rm -rf /tmp/Logs
     rm -rf /xDrip/FreeDNS_Fail
     if [ ! "`grep 'Could not authenticate' /tmp/hosts`" = "" ] # Failed to log in
     then
-      echo -e "Login to FreeDNS failed autherntication.      $(date)\n" | cat - /xDrip/FreeDNS_AutoLogin_Logs > /tmp/Logs
+      # Log.
+      /xDrip/scripts/AddLog.sh "Login to FreeDNS failed autherntication." /xDrip/FreeDNS_AutoLogin_Logs
       cat > /xDrip/FreeDNS_Fail << EOF
 FreeDNS failed authentication.
 EOF
     else
-      echo -e "Logged into FreeDNS.      $(date)\n" | cat - /xDrip/FreeDNS_AutoLogin_Logs > /tmp/Logs
+      # Log.
+      /xDrip/scripts/AddLog.sh "Logged into FreeDNS." /xDrip/FreeDNS_AutoLogin_Logs
     fi
   else
-    # Create a log.
-    echo -e "The /xDrip/FreeDNS_ID_Pass file does not exist.      $(date)\n" | cat - /xDrip/FreeDNS_AutoLogin_Logs > /tmp/Logs
+    # Log.
+    /xDrip/scripts/AddLog.sh "The /xDrip/FreeDNS_ID_Pass file does not exist." /xDrip/FreeDNS_AutoLogin_Logs
     cat > /xDrip/FreeDNS_Fail << EOF
 FreeDNS_ID_Pass file does not exist.
 EOF
 
   fi
 else
-  # Create a log.
-  rm -rf /tmp/Logs
-  echo -e "The FreeDNS site seems to be down.      $(date)\n" | cat - /xDrip/FreeDNS_AutoLogin_Logs > /tmp/Logs
+  # Log.
+  /xDrip/scripts/AddLog.sh "The FreeDNS site seems to be down." /xDrip/FreeDNS_AutoLogin_Logs
 fi
-# Finalize the log.
-sudo /bin/cp -f /tmp/Logs /xDrip/FreeDNS_AutoLogin_Logs
  
