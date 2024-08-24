07464d442259d68b881ceeb0974b5057caf07cf2
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-24 14:00:18 -0400

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -200,7 +200,7 @@ Disk size: $disksz        $DiskUsedPercent used \n\
 Ubuntu: $ubuntu \n\
 HTTP & HTTPS:  $http \n\
 ------------------------------------------ \n\
-Google Cloud Nightscout  2024.08.20\n\
+Google Cloud Nightscout  2024.08.26\n\
 $apisec_problem $Missing $Phase1 $rclocal_1 $freedns_id_pass \n\n\
 /$uname/$repo/$branch\n\
 Swap: $swap \n\
