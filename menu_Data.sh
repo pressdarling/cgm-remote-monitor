6724f4e845a3827b3407eb576960b6f412eeaf6e
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-13 11:40:23 -0500

    Update menu_Data.sh

--- menu_Data.sh
+++ menu_Data.sh
@@ -7,8 +7,7 @@ echo
 clear
 Choice=$(dialog --colors --nocancel --nook --menu "\
         \Zr Developed by the xDrip team \Zn\n\n\
-Use the arrow keys to move the cursor.\n\
-Press Enter to execute the highlighted option.\n" 14 50 4\
+Click OK or press Enter to select the highlighted option.\n" 14 50 4\
  "1" "Copy data from another Nightscout"\
  "2" "Backup MongoDB and variables"\
  "3" "Restore MongoDB and/or variables"\
@@ -36,3 +35,5 @@ sudo /xDrip/scripts/clone_nightscout.sh
 ;;
 
 esac
+
+  
