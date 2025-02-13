0833fa9c98d1083c7f0be6a9c5202d89cb7f467f
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-13 11:32:09 -0500

    Delete varserver.sh

--- varserver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dialog --colors --msgbox "     \Zr Developed the xDrip team \Zn\n\n\
-Copy the URL shown on the following page and paste it into a web browser. You will have 15 minutes to edit your variables in the browser." 10 50
-clear
-
-sudo /xDrip/ConfigServer/run_server_linux.sh
- 
