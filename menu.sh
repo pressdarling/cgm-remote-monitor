5b5248f4cb522921a29a30fd61b85f9e8702569e
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-13 11:30:59 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -8,10 +8,9 @@ while :
 do
 
 clear
-Choice=$(dialog --colors --nocancel --nook --menu "\
+Choice=$(dialog --colors --nocancel --help-button --menu "\
         \Zr Developed by the xDrip team \Zn\n\n
-Use the arrow keys to move the cursor.\n\
-Press Enter to execute the highlighted option.\n\n" 18 50 8\
+Click OK or press Enter to select the highlighted option.\n\n" 18 50 8\
  "1" "Status (may take up to 2 minutes.)"\
  "2" "Logs"\
  "3" "Google Cloud setup"\
@@ -21,6 +20,49 @@ Press Enter to execute the highlighted option.\n\n" 18 50 8\
  "7" "Reboot server (Nightscout)"\
  "8" "Exit to shell (terminal)"\
  3>&1 1>&2 2>&3)
+
+ response=$?
+ if [ $response = 2 ] # Help
+ then
+   clear
+   dialog --colors --no-shadow --infobox "Press any key to return to the main menu." 3 46
+
+   tput civis
+   printf '\e[1;44m%-6s\e[m' 
+   printf '\n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '                                                    \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '           Developed by the xDrip team              \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '                                                    \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf ' Click the links below to access the corresponding  \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf ' support pages.                                     \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '                                                    \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '                                                    \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Tips.html\e\\ Tips \e]8;;\e\\                                           \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '                                                    \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/FAQ.html\e\\ FAQ \e]8;;\e\\                                            \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '                                                    \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\ Troubleshooting \e]8;;\e\\                                \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '                                                    \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '   \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\ Contact us \e]8;;\e\\                                     \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '                                                    \n'
+   read -p "" -n1 -s
+   tput cnorm
+ fi
 
  clear
 
@@ -59,11 +101,7 @@ if [ $response = 255 ] || [ $response = 1 ]
 then
 clear
 else
-clear
-sudo reboot
-dialog --colors --pause "       \Zr Developed by the xDrip team \Zn\n\n\
-Please wait 25 seconds for the system to reboot. An expected error message will then appear. Wait an additional 30 seconds before attempting to reconnect." 14 50 25
-exit
+/xDrip/scripts/reboot.sh
 fi
 ;;
 
