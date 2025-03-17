3c879cd5f6f547f916b010aa32b513ed654cc802
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-16 21:08:52 -0400

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -22,6 +22,7 @@ fi
 whichpack=$(which gpg)
 if [ "$whichpack" = "" ]
 then
+  /xDrip/scripts/wait_4_completion.sh
   sudo apt-get -y install jq net-tools gnupg
   # The last item on the above list of packages must be verified in Status.sh to have been installed.
 fi 
@@ -44,6 +45,7 @@ then
   echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
   echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
+  /xDrip/scripts/wait_4_completion.sh
   systemctl start mongod
   systemctl enable mongod
 
