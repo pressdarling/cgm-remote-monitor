7fb5a48e5ec5c952472cbf84fd2d6c5a006eca6c
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-16 22:26:02 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -102,7 +102,7 @@ sudo git clone https://github.com/jamorham/nightscout-vps.git  # ✅✅✅✅✅
 ls > /tmp/repo
 sudo mv -f /tmp/repo .    # The repository name is now in /srv/repo
 cd "$(< repo)"
-sudo git checkout vps-dev  # ✅✅✅✅✅ Main - Uncomment before PR.
+sudo git checkout vps-2  # ✅✅✅✅✅ Main - Uncomment before PR.
 #sudo git checkout MoreWaits_Test  # ⛔⛔⛔⛔⛔ For test - Comment out before PR.
 
 sudo git branch > /tmp/branch
